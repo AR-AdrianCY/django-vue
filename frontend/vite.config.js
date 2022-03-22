dc167f31347259907b616a51795253317797eced
Adrian Chau <adrian.chau@augustrobotics.com>
2022-03-22 14:31:26 +0800

    first commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    manifest: true,
+    outDir: "../static/dist",
+  },
+  server: {
+    hmr: true
+  },
+  base: "/static",
+  root: "./",
+  plugins: [vue()]
+})
